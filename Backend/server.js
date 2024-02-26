68ad9b33c4e2e8b3c0d82e17206ae68992ce8a27
Ewando <55455275+Ewando@users.noreply.github.com>
2024-02-26 19:43:31 +0000

    adding gesture recognition

--- Backend/server.js
+++ Backend/server.js
@@ -4,23 +4,152 @@ const os = require('os');
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const Datastore = require('nedb');
+const usersDB = new Datastore({ filename: './data/users.db', autoload: true });
+const moduleConfigsDB = new Datastore({ filename: './data/moduleConfigs.db', autoload: true });
+const bcrypt = require('bcrypt');
 
+module.exports = usersDB;
 
+app.use(express.json());
 app.use(cors());
+
+// Get the IP of the system
 
 app.get('/getIP', (req, res) => {
     try {
         const networkInterfaces = os.networkInterfaces();
-        // Check if 'wlan0' interface exists, otherwise use an alternative
         const ip = networkInterfaces['wlan0'] ? networkInterfaces['wlan0'][0]['address'] : 'Interface not found';
         res.send({ ip });
     } catch (error) {
-        // Send a server error response
         res.status(500).send('Server error: ' + error.message);
     }
 });
 
+// Sign up a user
+
+app.post('/signup', async (req, res) => {
+
+    const { email, password, firstName, lastName } = req.body;
+
+    usersDB.findOne({ email }, async (err, user) => {
+        if (err) {
+            return res.status(500).send('Error accessing the database');
+        }
+        if (user) {
+            return res.status(400).send('Email already exists');
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        usersDB.insert({ email, password: hashedPassword, firstName, lastName }, (err, newUser) => {
+            if (err) {
+                return res.status(500).send('Error creating user');
+            }
+            res.status(201).send('User created successfully');
+        });
+    });
+});
+
+// Log a user in
+
+app.post('/login', (req, res) => {
+    const { email, password } = req.body;
+    usersDB.findOne({ email }, async (err, user) => {
+        if (err) {
+            return res.status(500).send('Error accessing the database');
+        }
+        if (!user || !(await bcrypt.compare(password, user.password))) {
+            return res.status(400).send('Invalid credentials');
+        }
+        res.send('Login successful');
+    });
+});
+
+// Get user via email  
+
+app.get('/user', (req, res) => {
+    const userEmail = req.query.email;
+
+    if (!userEmail) {
+        return res.status(400).send('Email parameter is required');
+    }
+
+    usersDB.findOne({ email: userEmail }, (err, user) => {
+        if (err) {
+            return res.status(500).send('Error accessing the database');
+        }
+        if (!user) {
+            return res.status(404).send('User not found');
+        }
+     
+        const { _id, firstName, lastName, email } = user;
+        res.send({ _id, firstName, lastName, email });
+    });
+});
+
+// Save module configs
+app.post('/saveModuleConfig', async (req, res) => {
+    const { userId, moduleConfig } = req.body;
+
+    if (!userId || !moduleConfig.moduleId) {
+        return res.status(400).send('Missing user ID or module configuration');
+    }
+
+    const query = { userId: userId, moduleId: moduleConfig.moduleId };
+    const update = { $set: { config: moduleConfig.config } }; // Adjust based on your actual config structure
+
+    moduleConfigsDB.findOne(query, (err, existingConfig) => {
+        if (err) {
+            return res.status(500).send('Error accessing the database');
+        }
+
+        if (existingConfig) {
+            // Update existing config
+            moduleConfigsDB.update(query, update, {}, (err) => {
+                if (err) {
+                    return res.status(500).send('Error updating configuration');
+                }
+                res.send('Configuration updated successfully');
+            });
+        } else {
+            // Insert new config
+            const newDoc = { ...query, config: moduleConfig.config }; // Combine query with config for new document
+            moduleConfigsDB.insert(newDoc, (err, newConfig) => {
+                if (err) {
+                    return res.status(500).send('Error saving module configuration');
+                }
+                res.status(201).send('Configuration saved successfully');
+            });
+        }
+    });
+});
+
+
+// Get module configs
+
+app.get('/getModuleConfig/:userId', (req, res) => {
+    const { userId } = req.params;
+
+    if (!userId) {
+        return res.status(400).send('User ID is required');
+    }
+
+    moduleConfigsDB.find({ userId: userId }, (err, configs) => {
+        if (err) {
+            return res.status(500).send('Error accessing the database');
+        }
+        if (configs.length === 0) {
+            return res.status(404).send('Configurations not found');
+        }
+        res.send(configs);
+    });
+});
+
+
+
 const port = 3001;
+
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
--- Mirror/src/App.js
+++ Mirror/src/App.js
@@ -1,19 +1,17 @@
 import './App.scss';
-import React, { useState, useEffect, useRef } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPersonWalking, faCircleExclamation, faFaceSmileBeam } from '@fortawesome/free-solid-svg-icons';
+import React, { useState, useEffect } from 'react';
 import 'animate.css';
 import axios from 'axios';
 import QRCode from 'qrcode.react'; 
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 
-
 function App() {
 
   const [currentDateTime, setCurrentDateTime] = useState(new Date());
-  const [isBright, setIsBright] = useState(false);
+  const [isBright, setIsBright] = useState(true);
   const [ip, setIp] = useState('');
+  const [activeCardIndex, setActiveCardIndex] = useState(0); // State for active card index
 
   useEffect(() => {
 
@@ -26,87 +24,126 @@ function App() {
       console.error('There was an error fetching the IP address:', error);
     });
 
-
     const timer = setInterval(() => {
       setCurrentDateTime(new Date());
     }, 60000);
 
-    const ws = new WebSocket('ws://192.168.1.82:5678');
-
-    ws.onopen = () => {
-      console.log('WebSocket connection established');
-    };
-
-    ws.onmessage = (event) => {
-      console.log('Message received: ', event.data);
-      if (event.data === "Motion detected") { // Ensure this matches exactly
-        setIsBright(true); // Toggle or set state based on the message
+    const handleKeyPress = (event) => {
+      if (event.key === 'ArrowUp') {
+        setActiveCardIndex(prevIndex => (prevIndex - 1 + 4) % 4); // Cycle up through 4 cards
+      } else if (event.key === 'ArrowDown') {
+        setActiveCardIndex(prevIndex => (prevIndex + 1) % 4); // Cycle down through 4 cards
       }
     };
 
-    ws.onclose = () => {
+    window.addEventListener('keydown', handleKeyPress);
+
+    const motionSocket = new WebSocket('ws://192.168.1.82:5678');
+    const gestureSocket = new WebSocket('ws://192.168.1.82:5679');
+
+    motionSocket.onopen = () => {
+      console.log('WebSocket connection established');
+    };
+
+    gestureSocket.onopen = () => {
+      console.log('WebSocket connection established');
+    };
+
+    motionSocket.onmessage = (event) => {
+      console.log('Message received: ', event.data);
+    
+      switch (event.data) {
+        case "Motion detected":
+          console.log("Motion Detected");
+          setIsBright(true); // Example of setting state based on motion detection
+          break;
+        default:
+          console.log("Unrecognized message or gesture");
+      }
+    };
+    
+    gestureSocket.onmessage = (event) => {
+      console.log('Message received: ', event.data);
+    
+      switch (event.data) {
+        case "Open Palm":
+          console.log("Open Palm gesture detected");
+          // Perform an action or update the state based on the Open Palm gesture
+          break;
+        case "Thumbs Up":
+          console.log("Thumbs Up gesture detected");
+          setActiveCardIndex(prevIndex => (prevIndex - 1 + 4) % 4); 
+          // Perform an action or update the state based on the Thumbs Up gesture
+          break;
+        case "Pointing Up":
+          console.log("Pointing Up gesture detected");
+          // Perform an action or update the state based on the Pointing Up gesture
+          break;
+        case "Thumbs Down":
+          console.log("Thumbs Down gesture detected");
+          // Perform an action or update the state based on the Thumbs Down gesture
+          break;
+        default:
+          console.log("Unrecognized message or gesture");
+      }
+    };
+
+    motionSocket.onclose = () => {
+      console.log('WebSocket connection closed');
+    };
+
+    gestureSocket.onclose = () => {
       console.log('WebSocket connection closed');
     };
 
     return () => {
       clearInterval(timer);
+      window.removeEventListener('keydown', handleKeyPress);
+      motionSocket.close();
+      gestureSocket.close();
     };
   }, []);
 
-  useEffect(() => {
-    
-  }, []);
-  
-  
-
-  const timeFormatOptions = {
-    hour: '2-digit',
-    minute: '2-digit'
-  };
-
-  const dateFormatOptions = {
-    weekday: 'long', 
-    day: 'numeric',
-    month: 'long'
+  const renderActiveCard = () => {
+    switch (activeCardIndex) {
+      case 0:
+        return <div className='contentCard'><p>Weather</p></div>;
+      case 1:
+        return <div className='contentCard'><p>Anecdotes</p></div>;
+      case 2:
+        return <div className='contentCard'><p>Ask Myra</p></div>;
+      case 3:
+        return <div className='contentCard'><p>Calendar</p><Calendar /></div>;
+      default:
+        return <div className='contentCard'><p>Unknown Card</p></div>;
+    }
   };
 
   return (
-
-    <>
-
     <div className={`App ${isBright ? 'brighten' : ''}`}>
-
-      <section className={`systemPrompts ${isBright ? 'brighten' : ''}`}>
-
-        <p className='hidden'><FontAwesomeIcon className='green' icon={faFaceSmileBeam}/> <span>Hello Ewan</span></p>
-        <p className='animate__animated animate__flash'><FontAwesomeIcon className='orange' icon={faPersonWalking}/> <span>Motion Detected</span></p>
-        <p className='animate__animated animate__flash hidden'><FontAwesomeIcon className='red' icon={faCircleExclamation}/> <span>Unknown user</span></p>
-        
-      </section>
-
-      <div className='dateTimeModule'> 
-          <div className="qrCodeContainer hidden">
-            <QRCode value='https://www.youtube.com/watch?v=dQw4w9WgXcQ' size={100}  />
-          </div>
-          <div className={`timeContainer ${isBright ? 'whiteBackground brighten' : ''}`}>
-            <h1>{currentDateTime.toLocaleTimeString([], timeFormatOptions)}</h1>
-            <h2>{currentDateTime.toLocaleDateString([], dateFormatOptions)}</h2>
-          </div>
+      <div className='headerBar'>
+        <div className="qrCodeContainer hidden">
+          <QRCode value='https://www.youtube.com/watch?v=dQw4w9WgXcQ' size={100} />
+        </div>
+        <div className={`timeContainer ${isBright ? 'whiteBackground brighten' : ''}`}>
+          <h2>{currentDateTime.toLocaleDateString([], { weekday: 'long', day: 'numeric', month: 'long' })}</h2>
+          <h1>{currentDateTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}</h1>
+        </div>
+        <div className='userContainer'>
+          <i className="fa-solid fa-user-slash"></i>
+          <p>No user detected</p>
+        </div>
       </div>
 
-
       <section className='contentSection'>
-       <Calendar />
+        {renderActiveCard()}
       </section>
-
-    </div>
 
       <section className={`userDetectionField ${isBright ? 'brighten' : ''}`}>
-       
       </section>
-
-    </>
+    </div>
   );
 }
 
 export default App;
+
